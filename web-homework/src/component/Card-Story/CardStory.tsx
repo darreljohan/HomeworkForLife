5199cf11d0cc32afa91a3cddc69829f08e854809
listenerservice <darreljohan162@gmail.com>
2025-01-05 17:25:11 +0700

    Base Ui Layout without Login and Register Form

--- web-homework/src/component/Card-Story/CardStory.tsx
+++ web-homework/src/component/Card-Story/CardStory.tsx
@@ -1,5 +1,21 @@
 import "./CardStory.css";
+import editLogo from "../../assets/edit.png";
 
-export default function CardStory() {
-  return <div className="CardStory"></div>;
+type CardProps = {
+  dateWritten: Date;
+  note: string;
+};
+
+export default function CardStory({ dateWritten, note }: CardProps) {
+  return (
+    <div className="cardStory">
+      <div className="dateSection">
+        {dateWritten.toDateString()}
+        <div className="editButton">
+          <img src={editLogo}></img>
+        </div>
+      </div>
+      <div className="noteSection">{note}</div>
+    </div>
+  );
 }
--- web-homework/src/component/Card-Story/CardStoryAdd.tsx
+++ web-homework/src/component/Card-Story/CardStoryAdd.tsx
@@ -1,5 +1,19 @@
+import dayjs from "dayjs";
 import "./CardStoryAdd.css";
+import editLogo from "../../assets/edit.png";
 
 export default function CardStoryAdd() {
-  return <div className="cardStoryAdd"></div>;
+  return (
+    <div className="cardStory">
+      <div className="dateSection">
+        <div>{dayjs().format("ddd MMM D YYYY").toString()}</div>
+        <div className="editButton">
+          <img src={editLogo}></img>
+        </div>
+      </div>
+      <div className="addSection">
+        <div>Let this day be written and remembered.</div>
+      </div>
+    </div>
+  );
 }
